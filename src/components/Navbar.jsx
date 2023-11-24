9d0a1de15ea88cc4adacdeb0ad87d49504b509ec
itsbluejelly <ronniedenzel0@gmail.com>
2023-11-24 17:35:10 +0300

    Merge pull request #3 from Brikita/main
    Use Swiper Js for the carousel in the homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
-/** @jsxImportSource theme-ui */
+/* @jsxImportSource theme-ui */
 // IMPORING NECESSARY MODULES
 
 import { NavLink } from "react-router-dom";
+
+import { useWeb3Modal } from "@web3modal/wagmi/react";
 import { useState, useContext } from "react";
 import NightlightIcon from '@mui/icons-material/Nightlight';
 import MenuIcon from "@mui/icons-material/Menu";
@@ -12,6 +14,7 @@ import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 //  IMPORING NECESSARY COMPONENTS
 import SearchIcon from "@mui/icons-material/Search";
 import LightModeIcon from "@mui/icons-material/LightMode";
+// import { Icon } from "@iconify/react";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { TextField } from "@mui/material";
@@ -19,13 +22,21 @@ import { ThemeContext } from "../contexts/ThemeProvider";
 
 // EXPORTING A FUNCTION THAT CREATES A NAVBAR COMPONENT
 export default function Navbar(props) {
-  // OBTAINING CONTEXT VALUES
+  const { open } = useWeb3Modal();
   const [mode, toggleTheme] = useContext(ThemeContext);
 
-  // A STATE TO KEEP TRACK OF WHEN TO SHOW ADDITIONAL BUTTONS
+  // Function to connect the wallet
+  const connectWallet = async () => {
+    try {
+      // Open the wallet modal using the open function from useWeb3Modal
+      await open();
+    } catch (error) {
+      console.error("Error connecting to wallet:", error);
+    }
+  };
+
   const [showAdditionalButtons, setShowAdditionalButtons] = useState(false);
   
-  // A FUNCTION TO SET THE ADDITIOMAL BUTTONS TO FALSE
   const toggleAdditionalButtons = () => {
     setShowAdditionalButtons(!showAdditionalButtons);
   };
@@ -80,9 +91,10 @@ export default function Navbar(props) {
           />
         </button>
 
+
         <button 
           className='w-[45px] h-[45px] rounded-full bg-small-screen-buttons__wallet-button border-[2px] border-solid border-small-screen-buttons__wallet-button p-[4px] cursor-pointer transition-all duration-500 ease-in-out flex justify-center items-center'
-          onClick={toggleAdditionalButtons}
+           onClick={toggleAdditionalButtons}
           title='show-wallet'
         >
           <AccountBalanceWalletIcon/>
@@ -163,9 +175,9 @@ export default function Navbar(props) {
         </div>
       </div>
 
-      <div className="extra-large-screen-buttons hidden lg:flex lg:justify-around">
-        <div className="lg:flex lg:items-center lg:justify-center">
-          <SearchIcon className="text-[#706f6f] cursor-pointer" />
+        <div className="extra-large-screen-buttons hidden lg:flex lg:justify-around">
+          <div className="lg:flex lg:items-center lg:justify-center">
+            <SearchIcon className="text-[#706f6f] cursor-pointer" />
 
           <TextField
             type="text"
@@ -176,152 +188,159 @@ export default function Navbar(props) {
           />
         </div>
 
-        <div className="nav-links  items-center flex my-0 mr-[10px] ml-[10.104px] p-0 pl-[0px] justify-evenly">
-          <div>
-            <NavLink
-              className="rewards m-0 mr-[32px] text-[#FFDB24] font-[Inter] font-[400] leading-normal text-[14px]"
-              to="/rewards"
+          <div className="nav-links  items-center flex my-0 mr-[10px] ml-[10.104px] p-0 pl-[0px] justify-evenly">
+            <div>
+              <NavLink
+                className="rewards m-0 mr-[32px] text-[#FFDB24] font-[Inter] font-[400] leading-normal text-[14px]"
+                to="/rewards"
+              >
+                Rewards
+              </NavLink>
+            </div>
+
+            <div className="dropdown">
+              <span className="m-0 mr-[32px]  text-[14px] font-[400] leading-normal">
+                <a className="">Explore</a>
+
+                <div className="dropdown-content hidden absolute text-[#555c69] bg-[ ] min-w-[160px] z-[1] rounded-[5px] ml-[-10px] shadow-dropdown-content">
+                 
+                  <NavLink
+                    className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
+                    to="/explore/allcollections"
+                  >
+                    All collections
+                  </NavLink>
+
+                  <NavLink
+                    className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
+                    to="/explore/allnfts"
+                  >
+                    All NFTs
+                  </NavLink>
+                  {/* Add more links as needed */}
+                </div>
+              </span>
+            </div>
+
+            <div className="dropdown">
+              <span className="m-0 mr-[32px]  text-[14px] font-[400] leading-normal">
+                <a className="">Creators</a>
+                <div className="dropdown-content hidden absolute text-[#555c69] bg-[ ] min-w-[160px] z-[1] rounded-[5px] ml-[-10px] shadow-dropdown-content">
+                  {/* Dropdown links */}
+                  <NavLink
+                    className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
+                    to="/creators/mining"
+                  >
+                    Create your collection
+                  </NavLink>
+
+                  <NavLink
+                    className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
+                    to="/creators/verification"
+                  >
+                    Verify your collection
+                  </NavLink>
+                  {/* Add more links as needed */}
+                </div>
+              </span>
+            </div>
+
+            <div className="dropdown">
+              <span className="m-0 mr-[32px]  text-[14px] font-[400] leading-normal">
+                <a className="">About</a>
+                <div className="dropdown-content hidden absolute text-[#555c69] bg-[ ] min-w-[160px] z-[1] rounded-[5px] ml-[-10px] shadow-dropdown-content">
+                  {/* Dropdown links */}
+                  <NavLink
+                    className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
+                    to="/about/team"
+                  >
+                    Our team
+                  </NavLink>
+
+                  <NavLink
+                    className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
+                    to="/about/impact"
+                  >
+                    Our impact
+                  </NavLink>
+                  {/* Add more links as needed */}
+                </div>
+              </span>
+            </div>
+
+            <div className="dropdown">
+              <span className="m-0 mr-[32px] text-[14px] font-[400] leading-normal">
+                <a className="">Launchpad</a>
+                <div className="dropdown-content hidden absolute text-[#555c69] bg-[ ] min-w-[160px] z-[1] rounded-[5px] ml-[-10px] shadow-dropdown-content">
+                  {/* Dropdown links */}
+                  <NavLink
+                    className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
+                    to="/launchpad/launches"
+                  >
+                    Launches
+                  </NavLink>
+
+                  <NavLink
+                    className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
+                    to="/launchpad/apply for launchpad"
+                  >
+                    Apply For Launchpad
+                  </NavLink>
+                  {/* Add more links as needed */}
+                </div>
+              </span>
+            </div>
+
+            <span className="m-0 mr-[20px] text-[#FFF] text-[14px] font-[400] leading-normal">
+              <button onClick={() => toggleTheme()}>
+                {mode === "dark" ? (
+                  <LightModeIcon className="text-[#FFDB24] cursor-pointer" />
+                ) : (
+                 <NightlightIcon className="text-[#9ca3af] cursor-pointer" />
+                )}
+              </button>
+            </span>
+
+            <span className="m-0 mr-[20px] text-[#FFF] text-[14px] font-[400] leading-normal">
+              <NotificationsIcon className="text-[#9CA3AF] cursor-pointer" />
+            </span>
+
+            <div
+              className="text-[#9CA3AF] text-[16px]"
+              onClick={props.toggleLanguage}
             >
-              Rewards
-            </NavLink>
-          </div>
+              EN
+            </div>
 
-          <div className="dropdown">
-            <span className="m-0 mr-[32px]  text-[14px] font-[400] leading-normal">
-              <a className="">Explore</a>
-
-              <div className="dropdown-content hidden absolute text-[#555c69] bg-[ ] min-w-[160px] z-[1] rounded-[5px] ml-[-10px] shadow-dropdown-content">
-                {/* Dropdown links */}
-                <NavLink
-                  className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
-                  to="/explore/allcollections"
-                >
-                  All collections
-                </NavLink>
-
-                <NavLink
-                  className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
-                  to="/explore/allnfts"
-                >
-                  All NFTs
-                </NavLink>
-                {/* Add more links as needed */}
-              </div>
-            </span>
-          </div>
-
-          <div className="dropdown">
-            <span className="m-0 mr-[32px]  text-[14px] font-[400] leading-normal">
-              <a className="">Creators</a>
-              <div className="dropdown-content hidden absolute text-[#555c69] bg-[ ] min-w-[160px] z-[1] rounded-[5px] ml-[-10px] shadow-dropdown-content">
-                {/* Dropdown links */}
-                <NavLink
-                  className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
-                  to="/creators/mining"
-                >
-                  Create your collection
-                </NavLink>
-
-                <NavLink
-                  className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
-                  to="/creators/verification"
-                >
-                  Verify your collection
-                </NavLink>
-                {/* Add more links as needed */}
-              </div>
-            </span>
-          </div>
-
-          <div className="dropdown">
-            <span className="m-0 mr-[32px]  text-[14px] font-[400] leading-normal">
-              <a className="">About</a>
-              <div className="dropdown-content hidden absolute text-[#555c69] bg-[ ] min-w-[160px] z-[1] rounded-[5px] ml-[-10px] shadow-dropdown-content">
-                {/* Dropdown links */}
-                <NavLink
-                  className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
-                  to="/about/team"
-                >
-                  Our team
-                </NavLink>
-
-                <NavLink
-                  className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
-                  to="/about/impact"
-                >
-                  Our impact
-                </NavLink>
-                {/* Add more links as needed */}
-              </div>
-            </span>
-          </div>
-
-          <div className="dropdown">
-            <span className="m-0 mr-[32px] text-[14px] font-[400] leading-normal">
-              <a className="">Launchpad</a>
-              <div className="dropdown-content hidden absolute text-[#555c69] bg-[ ] min-w-[160px] z-[1] rounded-[5px] ml-[-10px] shadow-dropdown-content">
-                {/* Dropdown links */}
-                <NavLink
-                  className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
-                  to="/launchpad/launches"
-                >
-                  Launches
-                </NavLink>
-
-                <NavLink
-                  className=" block py-[12px] px-[16px] hover:bg-[#2a2929]"
-                  to="/launchpad/apply for launchpad"
-                >
-                  Apply For Launchpad
-                </NavLink>
-                {/* Add more links as needed */}
-              </div>
-            </span>
-          </div>
-
-          <span className="m-0 mr-[20px] text-[#FFF] text-[14px] font-[400] leading-normal">
-            <button onClick={() => toggleTheme()}>
-              {mode === "dark" ? (
-                <LightModeIcon className="text-[#FFDB24] cursor-pointer" />
-              ) : (
-                <NightlightIcon className="text-[#9ca3af] cursor-pointer" />
-              )}
+            <button
+              className="text-[#000000] flex text-[14px] bg-[#FFDB24] py-[8px] px-[0px] border-none rounded-[8px] m-[0px] ml-[5px] transition-all duration-500 ease-in-out cursor-pointer active:scale-[0.8] active:opacity-[0.7]"
+              onClick={toggleAdditionalButtons}
+            >
+              <span>Connect wallet </span>
+              <span>
+                <KeyboardArrowDownIcon className="m-0 p-0 -ml-1" />
+              </span>
             </button>
-          </span>
-
-          <span className="m-0 mr-[20px] text-[#FFF] text-[14px] font-[400] leading-normal">
-            <NotificationsIcon className="text-[#9CA3AF] cursor-pointer" />
-          </span>
-
-          <div
-            className="text-[#9CA3AF] text-[16px]"
-            onClick={props.toggleLanguage}
-          >
-            EN
           </div>
+        </div>
+        {showAdditionalButtons && (
+          <div className="additional-buttons absolute top-14 right-0 mt-[10px] mr-5 flex flex-col space-y-2 ">
+            <button
+              onClick={props.toggleCardanoModal}
+              className="additional-button text-[#ffffff] text-[14px] bg-[#1864F8] py-[8px] px-[4px] border-none rounded-[8px]"
+            >
+              Cardano wallet
+            </button>
 
-          <button
-            className="text-[#000000] flex text-[14px] bg-[#FFDB24] py-[8px] px-[4px] border-none rounded-[8px] m-[0px] ml-[5px] transition-all duration-500 ease-in-out cursor-pointer active:scale-[0.8] active:opacity-[0.7]"
-            onClick={toggleAdditionalButtons}
-          >
-            <span>Connect wallet </span>
-            <span>
-              <KeyboardArrowDownIcon className="m-0 p-0 -ml-1" />
-            </span>
-          </button>
-        </div>
+            <button
+              onClick={connectWallet}
+              className="additional-button text-[#ffffff] text-[14px] bg-[#1864F8] py-[8px] px-[4px] border-none rounded-[8px]"
+            >
+              Etherium wallet
+            </button>
+          </div>
+        )}
       </div>
-      
-      {showAdditionalButtons && (
-        <div className="additional-buttons absolute top-14 right-0 mt-[10px] mr-5 flex flex-col space-y-2 ">
-          <button
-            className="additional-button text-[#ffffff] text-[14px] bg-[#1864F8] py-[8px] px-[4px] border-none rounded-[8px]"
-          >
-            Etherium wallet
-          </button>
-        </div>
-      )}
     </div>
-  </div>
   );
 }
--- src/pages/LaunchesPage.jsx
+++ src/pages/LaunchesPage.jsx
@@ -1,25 +1,23 @@
-// IMPORTING NECESSARY FILES
-    // IMPORTING NECESSARY MODULES
+
 import React from "react"
     // IMPORTING NECESSARY COMPONENTS
 import HeaderCarousel from "../components/HeaderCarousel"
-import LiveCollection from "../components/LaunchesPage/LiveCollection"
-import PastCollection from "../components/LaunchesPage/PastCollection"
+ import LiveCollection from "../components/LaunchesPage/LiveCollection"
+ import PastCollection from "../components/LaunchesPage/PastCollection"
 import CarouselEntry from "../components/LaunchesPage/CarouselEntry"
     // IMPORTING NECESSARY DATABASES
 import {launchpadCollectionsData} from '../database/launchpadCollectionsData'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-// A LAUNCHESPAGE FUNCTION THAT IS EXPORTED
+// Import Swiper styles
+import 'swiper/css';
+
 export default function LaunchesPage(){
-    // A BOOLEAN TO KEEP TRACK OF WHETHER OR NOT TO VIEW THE PAST COLLECTIONS
-    const [viewPastCollections, setViewPastCollections] = React.useState(false)
-    // A STATE TO HOLD THE CURRENT INDEX OF CARD DISPLAYED
     const [currentCard, setCurrentCard] = React.useState(0)
-
-    // OBTAINING THE DATA FROM DATABASE OF LIVE AND PAST COLLECTIONS
+    const [viewPastCollections, setViewPastCollections] = React.useState(false)
     const {liveCollections, pastCollections, carouselCollections} = launchpadCollectionsData
-
-    // A FUNCTION TO GENERATE AN ARRAY OF LIVE COLLECTIONS
+        // A FUNCTION TO GENERATE AN ARRAY OF LIVE COLLECTIONS
     function liveCollectionsGenerator() {
         const liveCollectionsArray = []
 
@@ -55,7 +53,6 @@ export default function LaunchesPage(){
         return pastCollectionsArray
     }
 
-    // A FUNCTION TO MOVE TO THE NEXT CARD
     function checkNextCard(){
         setCurrentCard(prevCard => prevCard < carouselCollections.length - 1 ? prevCard + 1 : 0)
     }
@@ -65,18 +62,18 @@ export default function LaunchesPage(){
         setCurrentCard(prevCard => prevCard > 0 ? prevCard - 1 : carouselCollections.length - 1)
     }
 
-    // AN ARRAY OF CAROUSELENTRIES
-    const generatedEntriesArray = carouselCollections.map(
-        detail => (<CarouselEntry 
-            key={detail._id}
-            image={detail.cardImage}
-            heading={detail.cardTitle}
-            description={detail.cardInfo}
-            id={detail._id}
-        />)
-    )
+    const generatedEntriesArray = carouselCollections.map(detail => (
+        <SwiperSlide key={detail._id}>
+            <CarouselEntry
+                image={detail.cardImage}
+                heading={detail.cardTitle}
+                description={detail.cardInfo}
+                id={detail._id}
+                
+            />
+        </SwiperSlide>
+    ));
 
-    // AN ARRAY OF DOTSELECTORS
     function generatedDotSelectorsArray(){
         const dotSelectorsArray = []
 
@@ -101,59 +98,81 @@ export default function LaunchesPage(){
         return dotSelectorsArray
     }
 
-    return(
-        <div className="min-h-[100vh] scroll-smooth box-border transition-all duration-500 ease-in-out">
-            <HeaderCarousel
-                carouselItems = {generatedEntriesArray}
-                currentCarouselItem = {currentCard}
-                dotScrollerButtons = {generatedDotSelectorsArray()}
-                checkPreviousItem = {() => checkPreviousCard()}
-                checkNextItem = {() => checkNextCard()}
-            />
 
-            <div className="flex items-center gap-[20px] my-0 mx-[20px] mt-[100px] w-full sm:w-[70%] sm:my-0 sm:mx-auto sm:mt-[150px] md:w-full md:ml-[12%] lg:mt-[50px] lg:gap-[50px]">
+  return (
+     <div>
+   
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+     
+      {generatedEntriesArray.map((entry, index) => (
+        <SwiperSlide key={index}
+        
+        > <HeaderCarousel
+        carouselItems = {generatedEntriesArray}
+        currentCarouselItem = {currentCard}
+        dotScrollerButtons = {generatedDotSelectorsArray()}
+        checkPreviousItem = {() => checkPreviousCard()}
+        checkNextItem = {() => checkNextCard()}
+    />
+       
+        </SwiperSlide>
+      ))}
+           
+          
+      
+    </Swiper>
+    <div className="flex items-center gap-[20px] my-0 mx-[20px] mt-[100px] w-full sm:w-[70%] sm:my-0 sm:mx-auto sm:mt-[150px] md:w-full md:ml-[12%] lg:mt-[50px] lg:gap-[50px]">
                 <p 
-                    onClick={() => setViewPastCollections(false)}
+                     onClick={() => setViewPastCollections(false)}
                     
-                    style={
-                        viewPastCollections 
-                            ? 
-                        null 
-                            : 
-                        {
-                            "backgroundColor": "#EE9E26",
-                            "border": "2px solid black",
-                            "borderRadius": "20px",
-                            "padding": "10px"
-                        }
-                    }
+                     style={
+                         viewPastCollections 
+                             ? 
+                         null 
+                             : 
+                         {
+                             "backgroundColor": "#EE9E26",
+                             "border": "2px solid black",
+                             "borderRadius": "20px",
+                             "padding": "10px"
+                         }
+                     }
 
-                    className="text-[#FFF] font-[Inter] text-[20px] not-italic font-[400] leading-normal cursor-pointer transition-all duration-500 ease-in-out"
-                >Live and upcoming</p>
+                     className="text-[#FFF] font-[Inter] text-[20px] not-italic font-[400] leading-normal cursor-pointer transition-all duration-500 ease-in-out"
+                 >Live and upcoming</p>
 
-                <p 
-                    onClick={() => setViewPastCollections(true)}
+                 <p 
+                     onClick={() => setViewPastCollections(true)}
                     
-                    style={
-                        viewPastCollections 
-                            ? 
-                        {
-                            "backgroundColor": "#EE9E26",
-                            "border": "2px solid black",
-                            "borderRadius": "20px",
-                            "padding": "10px"
-                        } 
-                            : 
-                        null
-                    }
+                     style={
+                         viewPastCollections 
+                             ? 
+                         {
+                             "backgroundColor": "#EE9E26",
+                             "border": "2px solid black",
+                             "borderRadius": "20px",
+                             "padding": "10px"
+                         } 
+                             : 
+                         null
+                     }
 
-                    className="text-[#FFF] font-[Inter] text-[20px] not-italic font-[400] leading-normal cursor-pointer transition-all duration-500 ease-in-out"
-                >Past</p>
-            </div>
+                     className="text-[#FFF] font-[Inter] text-[20px] not-italic font-[400] leading-normal cursor-pointer transition-all duration-500 ease-in-out"
+                 >Past</p>
+             </div>
 
-            <div className="grid grid-flow-row grid-cols-2 my-[80px] mx-auto py-0 px-[10px] gap-y-[20px] gap-x-[10px] w-full lg:grid-cols-3">
-                {viewPastCollections ? pastCollectionsGenerator() : liveCollectionsGenerator()}
-            </div>
-        </div>
-    )
+             <div className="grid grid-flow-row grid-cols-2 my-[80px] mx-auto py-0 px-[10px] gap-y-[20px] gap-x-[10px] w-full lg:grid-cols-3">
+                 {viewPastCollections ? pastCollectionsGenerator() : liveCollectionsGenerator()}
+             </div>
+         </div>
+    
+  
+  );
+  
 }
+
