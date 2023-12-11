334b1b9ba7b70cea2823a0cfdc5696e86dcd0209
Brian Kinyua <briankinyua0101@gmail.com>
2023-12-11 18:55:31 +0300

    fix padding

--- src/components/HomePage/WhaleActivityContainer.jsx
+++ src/components/HomePage/WhaleActivityContainer.jsx
@@ -60,7 +60,7 @@ export default function WhaleActivityContainer() {
         Whale activity
       </h1>
       <div className="flex w-11/12 justify-normal items-center">
-        <div className="flex w-full lg:w-4/5 px-4">
+        <div className="flex w-full lg:w-4/5 px-1">
           {whaleActivityCardArrayGenerator()}
         </div>
       </div>
